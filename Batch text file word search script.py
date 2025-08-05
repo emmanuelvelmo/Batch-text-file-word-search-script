84f25ba6015164c3390c3796a696eab84b9b7fd4
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-05 14:12:10 -0700

    Batch text file word search script.py

--- Batch text file word search script.py
+++ Batch text file word search script.py
@@ -4,24 +4,27 @@ import re
 # Lista de codificaciones a probar (en orden de prioridad)
 codificaciones_txt = ['utf-8', 'latin-1', 'utf-16', 'cp1252']
 
+# Bucle principal continuo
 while True:
-    directorio = input("Enter directory: ")
+    # Directorio de entrada
+    while True:
+        directorio_val = input("Enter directory: ").strip('"\'')
+
+        if os.path.isdir(directorio_val):
+            break
+
+        print("Wrong directory")
     
-    if not os.path.isdir(directorio):
-        print("Directory not found")
-        
-        continue
-    
-    palabra = input("Enter text to search: ")
+    palabra_val = input("Enter text to search: ")
     print("------------------------------------")
     
     # Búsqueda insensible a mayúsculas/minúsculas:
-    patron_busqueda = re.compile(r'\b' + re.escape(palabra) + r'\b', flags = re.IGNORECASE)
+    patron_busqueda = re.compile(r'\b' + re.escape(palabra_val) + r'\b', flags = re.IGNORECASE)
     
     # Buscar en todos los archivos del directorio
-    for raiz, _, archivos in os.walk(directorio):
-        for archivo_iter in archivos:
-            ruta_completa = os.path.join(raiz, archivo_iter)
+    for raiz_val, _, archivos_val in os.walk(directorio_val):
+        for archivo_iter in archivos_val:
+            ruta_completa = os.path.join(raiz_val, archivo_iter)
             
              # Probar diferentes codificaciones
             for codificacion_iter in codificaciones_txt:
